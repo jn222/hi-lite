3860864844033b1d349bbfc94b0f46a217ab59d8
jn222 <jonathanng222@gmail.com>
2023-09-15 20:25:04 -0400

    delete lib

--- lib/iron-config.ts
+++
@@ -1,9 +0,0 @@
-import { IronSessionOptions } from "iron-session";
-
-export const sessionOptions: IronSessionOptions = {
-  password: process.env.SECRET_COOKIE_PASSWORD as string,
-  cookieName: "iron-session/examples/next.js",
-  cookieOptions: {
-    secure: process.env.NODE_ENV === "production",
-  },
-}
